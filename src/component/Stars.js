75f29a72a73c744eb16d3c1ce189c11fd523fdaa
Liam Cavens <liamcavens@googlemail.com>
2018-08-08 17:23:54 +0100

    added for loop to stars

--- src/component/Stars.js
+++ src/component/Stars.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 
 
 const Stars = (props) => {
+    const numberOfStars = 5;
+
+    let stars = [];
+
+    for (let i = 0; i < numberOfStars ; i++) {
+        stars.push(<i key={i} className="glyphicon glyphicon-star"></i>)
+    }
+
     return (
         <div className="col-5">
-            <i className="glyphicon glyphicon-star"></i>
-            <i className="glyphicon glyphicon-star"></i>
-            <i className="glyphicon glyphicon-star"></i>
-            <i className="glyphicon glyphicon-star"></i>
+            {stars}
         </div>
     )
 }
